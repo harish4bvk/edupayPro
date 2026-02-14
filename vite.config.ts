1abd564cc5d04bf1200a769e1de8d20be76323b3
V K Harish Bodapati <32860768+harish4bvk@users.noreply.github.com>
2026-02-14 23:00:01 +0530

    Set build output directory to 'build'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    build: {
+    outDir: 'build'
+  }
     return {
       server: {
         port: 3000,
